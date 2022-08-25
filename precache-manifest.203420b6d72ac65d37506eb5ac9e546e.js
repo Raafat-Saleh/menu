cd886d9fad7db193649a0028a7681e9e3c01a16f
Raafat-Saleh <rafat.saleh1313@gmail.com>
2022-08-25 09:53:04 +0200

    Updates

---
+++ precache-manifest.203420b6d72ac65d37506eb5ac9e546e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37fd88614e9353a393503624362f498a",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "c119c50e76dd6116db2a",
+    "url": "/menu/static/css/main.14f5e0cb.chunk.css"
+  },
+  {
+    "revision": "d6da81e89034037073de",
+    "url": "/menu/static/js/2.d62a9384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.d62a9384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c119c50e76dd6116db2a",
+    "url": "/menu/static/js/main.2c54d8fa.chunk.js"
+  },
+  {
+    "revision": "2acabc264f1fa501847b",
+    "url": "/menu/static/js/runtime-main.bab3c470.js"
+  }
+]);
